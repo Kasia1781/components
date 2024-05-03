5f55bb60185d2385654b77d91a9b16b0a4def9c4
Kasia <kasia1781@gmail.com>
2024-05-03 19:10:46 +0200

    custom container component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import './App.css';
 import Button from './components/Button';
+import Container from './components/Container';
 import Input from './components/Input';
 
 function App() {
 	return (
 		<main>
+			<Container
+				as={Button}
+				onClick={() => {
+					console.log('ok');
+				}}>
+				Click me
+			</Container>
 			<p>
 				<Button>A Button</Button>
 			</p>
