282013382a11c5543fd25485ed5c237df06f5bd5
Kasia <kasia1781@gmail.com>
2024-05-03 18:52:44 +0200

    custom button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import './App.css';
+import Button from './components/Button';
 import Input from './components/Input';
 
 function App() {
 	return (
 		<main>
+			<p>
+				<Button>A Button</Button>
+			</p>
+			<p>
+				<Button href='https://google.com'>A Link</Button>
+			</p>
 			<Input id='name' label='Your name' type='text' />
 			<Input id='age' label='Your age' type='number' />
 		</main>
